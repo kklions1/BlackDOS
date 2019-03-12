9db6614896c03d7d9e407172361f094b54ce5e70
kklions1 <kklions1@gmail.com>
2019-03-12 16:41:12 -0400

    added stuff for the last step of lab 5

--- compileOS.sh
+++ compileOS.sh
@@ -5,3 +5,8 @@ dd if=config of=floppya.img bs=512 count=1 seek=258 conv=notrunc
 bcc -ansi -c -o kernel.o kernel.c
 ld86 -o kernel -d kernel.o kasm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
+
+./loadFile kitty1
+./loadFile kitty2
+./loadFile Stenv
+./loadFile fib
